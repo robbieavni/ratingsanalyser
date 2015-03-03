5ce6cbce85d9b3f395c6f4ca181ba8551c9f1d90
Robbie Avni <robbieavni@gmail.com>
2015-03-03 21:48:54 +0000

    adds more aggregate rating model manager methods

--- ratings/managers.py
+++ ratings/managers.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class RatingManager(models.Manager):
     def average_for_user(self, user_id):
-        return self.all().aggregate(models.Avg('rating'))['rating__avg']
+        return self.filter(user_id=user_id).aggregate(models.Avg('rating'))['rating__avg']
+
+    def imdb_average_for_users_films(self, user_id):
+        return self.filter(user_id=user_id).aggregate(models.Avg('film__imdb_rating'))['film__imdb_rating__avg']
+
+    def average_runtime_for_user(self, user_id):
+        return self.filter(user_id=user_id).aggregate(models.Avg('film__runtime'))['film__runtime__avg']
--- ratings/test_managers.py
+++ ratings/test_managers.py
@@ -6,6 +6,13 @@ from ratings.models import Rating
 class RatingManagerTestCase(TransactionTestCase):
     fixtures = ['me.json']
 
-    def test_aggregates(self):
+    def test_user_aggregate_rating(self):
         """Successfully calculates aggregates for a user's ratings"""
         self.assertEqual(Rating.objects.average_for_user('ur9663707'), 7.0036)
+
+    def test_imdb_aggregate_for_user(self):
+        """Successfully calculates aggregate of the IMDB ratings for a user's films"""
+        self.assertEqual(Rating.objects.imdb_average_for_users_films('ur9663707'), 7.27038)
+
+    def test_runtime_aggregate_for_user(self):
+        self.assertEqual(Rating.objects.average_runtime_for_user('ur9663707'),109.9587)
