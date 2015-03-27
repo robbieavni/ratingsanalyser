be52f72854cae6af23856a13bef45de86505ba5c
Robbie Avni <robbieavni@gmail.com>
2015-03-27 21:28:53 +0000

    manager with tests for most popular / most hipster films rated

--- ratings/managers.py
+++ ratings/managers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from itertools import chain
 
 
 class RatingManager(models.Manager):
@@ -27,3 +28,18 @@ class RatingManager(models.Manager):
             dictionary[decade][1] = decade_queryset.aggregate(models.Avg('rating'))['rating__avg']
         return dictionary
 
+    # Returns from the films a user has rated the 3 with the largest number of ratings / 3 with smallest
+    def hipster_films(self, user):
+        top_three = self.filter(user=user).order_by('film__number_of_votes')[0:3]
+        bottom_three = self.filter(user=user).order_by('-film__number_of_votes')[0:3]
+        high_and_low = chain(top_three, bottom_three)
+        hipster_list = []
+        for item in high_and_low:
+            inner_dict = {}
+            inner_dict['title'] = item.film.title
+            inner_dict['votes'] = item.film.number_of_votes
+            inner_dict['id'] = item.film.imdb_id
+            inner_dict['imdb_rating'] = item.film.imdb_rating
+            inner_dict['user_rating'] = item.rating
+            hipster_list.append(inner_dict)
+        return hipster_list
